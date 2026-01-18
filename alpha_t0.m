a5f65010745afef464ddce2ae84338cb7dfd75b5
Vincent Huynh <vincenthuynh22@gmail.com>
2026-01-18 13:32:43 -0800

    Remove plotting

--- alpha_t0.m
+++ alpha_t0.m
@@ -765,219 +765,219 @@ else
 end
 
 %% Plotting
-params_latex = ["\alpha","t_0"];
-params_text = ["alpha","t0"];
-color_vec = ["#A2142F", "#0072BD", "#333333"];
-
-home_dir = char(java.lang.System.getProperty('user.home'));
-folder_path = home_dir + "\Desktop\Updated Final AirComp Results\";
-
-plots = true;
-save_figs = true;
-
-if plots
-    % Linear plots
-    if experiment ~= "cwe_joint_grid"
-        for agent_db_idx = 1:length(agent_db_values)
-            % Create save directory if it does not exist.
-            save_folder = folder_path + experiment + "_" + string(agent_db_values(agent_db_idx)) + "_db";
-            if ~exist(save_folder, 'dir')
-                mkdir(save_folder);
-            end
-        
-            for param_idx = 1:2
-                % Collect all plot values for y-axis scaling.
-                all_vals = [avg_dep_var(:,agent_db_idx,:,param_idx,:,1:sensor_dimension);...
-                            avg_dep_mse(:,agent_db_idx,:,param_idx,:,1:sensor_dimension);...
-                            avg_dep_crlb(:,agent_db_idx,:,param_idx,:,1:sensor_dimension)];
-                for scheme_idx = 1:length(selected_schemes)
-                    scheme = selected_schemes(scheme_idx);
-                    fig1 = figure;
-                    set(fig1,'Position',[100,100,450,450])
-                    ax = gca;
-                    hold on
-                    
-                    % Plot pseudoplots for legend symbols.
-                    if experiment == "random_dropout"
-                        for dropout_idx = 1:length(dropout_vals)
-                            plot(nan,nan,'color',color_vec(dropout_idx),'LineWidth',2);
-                        end
-                    else
-                        for sensor_idx = 1:length(sensor_vals) 
-                            plot(nan,nan,'color',color_vec(sensor_idx),'LineWidth',2);
-                        end
-                    end
-                    plot(nan,nan,'^','color','black');
-                    plot(nan,nan,'x','color','black');
-                    plot(nan,nan,'o','color','black');
-    
-                    x_axis_series = channel_db_values(scheme_idx,:,agent_db_idx);
-                    
-                    if experiment == "random_dropout"
-                        count_vals = dropout_vals;
-                    else
-                        count_vals = sensor_vals;
-                    end
-    
-                    if scheme == "NPC"
-                        count_vals = count_vals(1);
-                    end
-    
-                    for count_idx = 1:length(count_vals)
-                        plot(x_axis_series,(squeeze(avg_dep_var(:,agent_db_idx,:,param_idx,scheme_idx,count_idx,1))),'-^','color',color_vec(count_idx),'LineWidth', 1)
-                        plot(x_axis_series,(squeeze(avg_dep_mse(:,agent_db_idx,:,param_idx,scheme_idx,count_idx,1))),'-x','color',color_vec(count_idx),'LineWidth', 1)
-                        plot(x_axis_series,(squeeze(avg_dep_crlb(:,agent_db_idx,:,param_idx,scheme_idx,count_idx,1))),'--o','color',color_vec(count_idx),'LineWidth', 1)
-                    end
-    
-                    xlabel('Channel SNR (dB)')
-                    ylabel(" ")
-        
-                    title('$$\hat{'+params_latex(param_idx)+'}$$','Interpreter','latex')
-                    
-                    set(ax, 'FontSize', 15);
-                    set(ax, 'YScale', 'log')
-    
-                    if scheme == "MPC"
-                        if experiment == "random_dropout"
-                            legend(["Drop = " + dropout_vals,"VAR","MSE","CRLB"], 'Location', 'best')
-                        else
-                            legend(["S = " + sensor_vals,"VAR","MSE","CRLB"], 'Location', 'best')
-                        end
-                    end
-                    
-                    % Add box around plot axes.
-                    box(ax, 'on');
-                    grid on
-    
-                    y_lower = 10^(-0.25)*min(all_vals,[],"all");
-                    y_upper = 1.05*max(all_vals,[],"all");
-                    ylim([y_lower,y_upper]);
-                    y_ticks = power_10_range(y_lower,y_upper);
-    
-                    % Don't plot for certain combinations due to axis and box
-                    % overlap
-                    if param_idx == 1
-                            y_ticks = y_ticks(2:end);
-                    end
-    
-                    ax.YTick = y_ticks;
-                    ax.GridColor = [0 0 0];
-                    ax.GridAlpha = 0.5;
-                    ax.LineWidth = 1;
-    
-                    % Save the figure.
-                    if save_figs
-                        exportgraphics(fig1, fullfile(save_folder, sprintf(experiment + "_" + string(agent_db_values(agent_db_idx)) + "_db_" + params_text(param_idx) + "_" + selected_schemes(scheme_idx) + ".png")), 'Resolution', 300);
-                    end
-                    % close(fig1)
-                end
-            end
-        end
-    % Surface plots
-    else
-        % Create save directory if it does not exist.
-        save_folder = folder_path + experiment + "_" + string(agent_db_values(end)) + "_db";
-        if ~exist(save_folder, 'dir')
-            mkdir(save_folder);
-        end
-    
-        % Toggle if you want to convert meshgrid values to log (you should).
-        convert_to_log = true;
-    
-        for param_idx = 1:2
-            % Collect all plot values for y-axis scaling.
-            all_vals = [avg_dep_var(:,:,:,param_idx,:,1:sensor_dimension);...
-                        avg_dep_mse(:,:,:,param_idx,:,1:sensor_dimension);...
-                        avg_dep_crlb(:,:,:,param_idx,:,1:sensor_dimension)];
-            if convert_to_log
-                all_vals = log10(all_vals);
-            end
-    
-            for scheme_idx = 1:length(selected_schemes)
-                fig1 = figure;
-                set(fig1,'Position',[100,100,675,475])
-                ax = gca;
-    
-                count_vals = sensor_vals;
-    
-                for count_idx = 1:length(count_vals)
-                    z_vals = squeeze(avg_dep_var(:,:,:,param_idx,scheme_idx,count_idx,1));
-                    if convert_to_log
-                        z_vals = log10(z_vals);
-                    end
-                    surf(agent_db_values, channel_db_values(1,:,1), z_vals)
-                end
-    
-                cb = colorbar;
-                colormap(turbo)
-                % Change view angle to better see both x and y dimensions
-                view(150,20)
-                xl = xlabel('Channel SNR (dB)', 'FontWeight', 'bold');
-                yl = ylabel('Sensor SNR (dB)', 'FontWeight', 'bold');
-                
-               
-                title('$$\hat{' + params_latex(param_idx) + '}$$', 'Interpreter','latex')
-                set(ax, 'FontSize', 15);
-    
-                zlabel('$$ \mathbf{\log_{10} ((\mathrm{Var} ( \hat{'+params_latex(param_idx)+'} ))}$$','Interpreter','latex', 'FontSize', 20)
-                
-                % Add box around plot axes.
-                box(ax, 'on');
-                grid on
-                
-                if convert_to_log
-                    z_lower = 1.05*min(all_vals,[],"all");
-                else
-                    set(ax, 'ZScale', 'log')
-                    z_lower = 10^(-0.25)*min(all_vals,[],"all");
-                end
-    
-                z_upper = 1.05*max(all_vals,[],"all");
-                
-                zlim([z_lower,z_upper]);
-                clim([z_lower,z_upper])
-    
-                if ~convert_to_log
-                    z_ticks = power_10_range(z_lower,z_upper);
-                    ax.ZTick = z_ticks;
-                end
-    
-                ax.GridColor = [0 0 0];
-                ax.GridAlpha = 1;
-                ax.LineWidth = 1;
-    
-                xpos = get(ax, 'Xlabel');
-                xpos = xpos.Position;
-                xpos(1) = xpos(1) + 2;
-                xpos(2) = xpos(2) - 2;
-                
-                xl.Position = xpos;
-                xl.Rotation = 10;
-    
-                ypos = get(ax, 'Ylabel');
-                ypos = ypos.Position;
-                ypos(1) = ypos(1) - 2;
-                ypos(2) = ypos(2) + 2.5;
-    
-                % if scheme_idx == 1
-                %     ypos(1) = ypos(1) - 5.5;
-                %     ypos(2) = ypos(2) - 2.5;
-                % else
-                %     ypos(1) = ypos(1) - 4.5;
-                %     ypos(2) = ypos(2) - 2.5;
-                % end
-    
-                yl.Position = ypos;
-                yl.Rotation = -30;
-    
-                axis normal
-    
-                % Save the figure.
-                if save_figs
-                    exportgraphics(fig1, fullfile(save_folder, sprintf(experiment + "_" + string(agent_db_values(end)) + "_db_" + params_text(param_idx) + "_" + selected_schemes(scheme_idx) + ".png")), 'Resolution', 300);
-                end
-                % close(fig1)
-            end
-        end
-    end
-end
+% params_latex = ["\alpha","t_0"];
+% params_text = ["alpha","t0"];
+% color_vec = ["#A2142F", "#0072BD", "#333333"];
+% 
+% home_dir = char(java.lang.System.getProperty('user.home'));
+% folder_path = home_dir + "\Desktop\Updated Final AirComp Results\";
+% 
+% plots = true;
+% save_figs = true;
+% 
+% if plots
+%     % Linear plots
+%     if experiment ~= "cwe_joint_grid"
+%         for agent_db_idx = 1:length(agent_db_values)
+%             % Create save directory if it does not exist.
+%             save_folder = folder_path + experiment + "_" + string(agent_db_values(agent_db_idx)) + "_db";
+%             if ~exist(save_folder, 'dir')
+%                 mkdir(save_folder);
+%             end
+% 
+%             for param_idx = 1:2
+%                 % Collect all plot values for y-axis scaling.
+%                 all_vals = [avg_dep_var(:,agent_db_idx,:,param_idx,:,1:sensor_dimension);...
+%                             avg_dep_mse(:,agent_db_idx,:,param_idx,:,1:sensor_dimension);...
+%                             avg_dep_crlb(:,agent_db_idx,:,param_idx,:,1:sensor_dimension)];
+%                 for scheme_idx = 1:length(selected_schemes)
+%                     scheme = selected_schemes(scheme_idx);
+%                     fig1 = figure;
+%                     set(fig1,'Position',[100,100,450,450])
+%                     ax = gca;
+%                     hold on
+% 
+%                     % Plot pseudoplots for legend symbols.
+%                     if experiment == "random_dropout"
+%                         for dropout_idx = 1:length(dropout_vals)
+%                             plot(nan,nan,'color',color_vec(dropout_idx),'LineWidth',2);
+%                         end
+%                     else
+%                         for sensor_idx = 1:length(sensor_vals) 
+%                             plot(nan,nan,'color',color_vec(sensor_idx),'LineWidth',2);
+%                         end
+%                     end
+%                     plot(nan,nan,'^','color','black');
+%                     plot(nan,nan,'x','color','black');
+%                     plot(nan,nan,'o','color','black');
+% 
+%                     x_axis_series = channel_db_values(scheme_idx,:,agent_db_idx);
+% 
+%                     if experiment == "random_dropout"
+%                         count_vals = dropout_vals;
+%                     else
+%                         count_vals = sensor_vals;
+%                     end
+% 
+%                     if scheme == "NPC"
+%                         count_vals = count_vals(1);
+%                     end
+% 
+%                     for count_idx = 1:length(count_vals)
+%                         plot(x_axis_series,(squeeze(avg_dep_var(:,agent_db_idx,:,param_idx,scheme_idx,count_idx,1))),'-^','color',color_vec(count_idx),'LineWidth', 1)
+%                         plot(x_axis_series,(squeeze(avg_dep_mse(:,agent_db_idx,:,param_idx,scheme_idx,count_idx,1))),'-x','color',color_vec(count_idx),'LineWidth', 1)
+%                         plot(x_axis_series,(squeeze(avg_dep_crlb(:,agent_db_idx,:,param_idx,scheme_idx,count_idx,1))),'--o','color',color_vec(count_idx),'LineWidth', 1)
+%                     end
+% 
+%                     xlabel('Channel SNR (dB)')
+%                     ylabel(" ")
+% 
+%                     title('$$\hat{'+params_latex(param_idx)+'}$$','Interpreter','latex')
+% 
+%                     set(ax, 'FontSize', 15);
+%                     set(ax, 'YScale', 'log')
+% 
+%                     if scheme == "MPC"
+%                         if experiment == "random_dropout"
+%                             legend(["Drop = " + dropout_vals,"VAR","MSE","CRLB"], 'Location', 'best')
+%                         else
+%                             legend(["S = " + sensor_vals,"VAR","MSE","CRLB"], 'Location', 'best')
+%                         end
+%                     end
+% 
+%                     % Add box around plot axes.
+%                     box(ax, 'on');
+%                     grid on
+% 
+%                     y_lower = 10^(-0.25)*min(all_vals,[],"all");
+%                     y_upper = 1.05*max(all_vals,[],"all");
+%                     ylim([y_lower,y_upper]);
+%                     y_ticks = power_10_range(y_lower,y_upper);
+% 
+%                     % Don't plot for certain combinations due to axis and box
+%                     % overlap
+%                     if param_idx == 1
+%                             y_ticks = y_ticks(2:end);
+%                     end
+% 
+%                     ax.YTick = y_ticks;
+%                     ax.GridColor = [0 0 0];
+%                     ax.GridAlpha = 0.5;
+%                     ax.LineWidth = 1;
+% 
+%                     % Save the figure.
+%                     if save_figs
+%                         exportgraphics(fig1, fullfile(save_folder, sprintf(experiment + "_" + string(agent_db_values(agent_db_idx)) + "_db_" + params_text(param_idx) + "_" + selected_schemes(scheme_idx) + ".png")), 'Resolution', 300);
+%                     end
+%                     % close(fig1)
+%                 end
+%             end
+%         end
+%     % Surface plots
+%     else
+%         % Create save directory if it does not exist.
+%         save_folder = folder_path + experiment + "_" + string(agent_db_values(end)) + "_db";
+%         if ~exist(save_folder, 'dir')
+%             mkdir(save_folder);
+%         end
+% 
+%         % Toggle if you want to convert meshgrid values to log (you should).
+%         convert_to_log = true;
+% 
+%         for param_idx = 1:2
+%             % Collect all plot values for y-axis scaling.
+%             all_vals = [avg_dep_var(:,:,:,param_idx,:,1:sensor_dimension);...
+%                         avg_dep_mse(:,:,:,param_idx,:,1:sensor_dimension);...
+%                         avg_dep_crlb(:,:,:,param_idx,:,1:sensor_dimension)];
+%             if convert_to_log
+%                 all_vals = log10(all_vals);
+%             end
+% 
+%             for scheme_idx = 1:length(selected_schemes)
+%                 fig1 = figure;
+%                 set(fig1,'Position',[100,100,675,475])
+%                 ax = gca;
+% 
+%                 count_vals = sensor_vals;
+% 
+%                 for count_idx = 1:length(count_vals)
+%                     z_vals = squeeze(avg_dep_var(:,:,:,param_idx,scheme_idx,count_idx,1));
+%                     if convert_to_log
+%                         z_vals = log10(z_vals);
+%                     end
+%                     surf(agent_db_values, channel_db_values(1,:,1), z_vals)
+%                 end
+% 
+%                 cb = colorbar;
+%                 colormap(turbo)
+%                 % Change view angle to better see both x and y dimensions
+%                 view(150,20)
+%                 xl = xlabel('Channel SNR (dB)', 'FontWeight', 'bold');
+%                 yl = ylabel('Sensor SNR (dB)', 'FontWeight', 'bold');
+% 
+% 
+%                 title('$$\hat{' + params_latex(param_idx) + '}$$', 'Interpreter','latex')
+%                 set(ax, 'FontSize', 15);
+% 
+%                 zlabel('$$ \mathbf{\log_{10} ((\mathrm{Var} ( \hat{'+params_latex(param_idx)+'} ))}$$','Interpreter','latex', 'FontSize', 20)
+% 
+%                 % Add box around plot axes.
+%                 box(ax, 'on');
+%                 grid on
+% 
+%                 if convert_to_log
+%                     z_lower = 1.05*min(all_vals,[],"all");
+%                 else
+%                     set(ax, 'ZScale', 'log')
+%                     z_lower = 10^(-0.25)*min(all_vals,[],"all");
+%                 end
+% 
+%                 z_upper = 1.05*max(all_vals,[],"all");
+% 
+%                 zlim([z_lower,z_upper]);
+%                 clim([z_lower,z_upper])
+% 
+%                 if ~convert_to_log
+%                     z_ticks = power_10_range(z_lower,z_upper);
+%                     ax.ZTick = z_ticks;
+%                 end
+% 
+%                 ax.GridColor = [0 0 0];
+%                 ax.GridAlpha = 1;
+%                 ax.LineWidth = 1;
+% 
+%                 xpos = get(ax, 'Xlabel');
+%                 xpos = xpos.Position;
+%                 xpos(1) = xpos(1) + 2;
+%                 xpos(2) = xpos(2) - 2;
+% 
+%                 xl.Position = xpos;
+%                 xl.Rotation = 10;
+% 
+%                 ypos = get(ax, 'Ylabel');
+%                 ypos = ypos.Position;
+%                 ypos(1) = ypos(1) - 2;
+%                 ypos(2) = ypos(2) + 2.5;
+% 
+%                 % if scheme_idx == 1
+%                 %     ypos(1) = ypos(1) - 5.5;
+%                 %     ypos(2) = ypos(2) - 2.5;
+%                 % else
+%                 %     ypos(1) = ypos(1) - 4.5;
+%                 %     ypos(2) = ypos(2) - 2.5;
+%                 % end
+% 
+%                 yl.Position = ypos;
+%                 yl.Rotation = -30;
+% 
+%                 axis normal
+% 
+%                 % Save the figure.
+%                 if save_figs
+%                     exportgraphics(fig1, fullfile(save_folder, sprintf(experiment + "_" + string(agent_db_values(end)) + "_db_" + params_text(param_idx) + "_" + selected_schemes(scheme_idx) + ".png")), 'Resolution', 300);
+%                 end
+%                 % close(fig1)
+%             end
+%         end
+%     end
+% end
